063f9341e11cb3e8b71887f67d638e75b47d3667
VitaliyDmitrienko <vddjack2008@yandex.ru>
2024-03-02 14:12:31 +0300

    Init.

---
+++ src/main/java/com/example/sheduler/HospitalTimeShedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.sheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalTimeShedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalTimeShedulerApplication.class, args);
+	}
+
+}
